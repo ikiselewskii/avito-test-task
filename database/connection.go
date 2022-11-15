c2e61b0e5001d1b9b0c2279ea8636f588bb1cc8c
ilya <i.kiselevsky@gmail.com>
2022-11-15 22:06:47 +0300

    general refactoring, makes it easier to read and write code

---
+++ database/connection.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+)
+
+func InitializeDBConnection(dsn string) (db *bun.DB) {
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	db = bun.NewDB(sqldb, pgdialect.New())
+	db.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.WithVerbose(true),
+		bundebug.FromEnv("BUNDEBUG"),
+	))
+	return
+}
--- main.go
+++ main.go
@@ -1,94 +1,15 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"github.com/gin-gonic/gin"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
-	"github.com/uptrace/bun/extra/bundebug"
-	"net/http"
-	"os"
-	"time"
+	"github.com/ikiselewskii/avito-test-task/database"
+	"github.com/ikiselewskii/avito-test-task/utils"
+	"github.com/ikiselewskii/avito-test-task/webserver"
 )
 
 func main() {
-	router := gin.Default()
-	InitializeEndpoints(router)
-	go router.Run()
-	db := InitializeDBConnection(os.Getenv("DSN"))
-	CreateUserTable(db)
-	CreateReservationsTable(db)
-
-}
-
-func InitializeEndpoints(r *gin.Engine) {
-	r.GET("/", rootRoute)
-	r.GET("/ping", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-}
-
-func InitializeDBConnection(dsn string) (db *bun.DB) {
-	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
-	db = bun.NewDB(sqldb, pgdialect.New())
-	db.AddQueryHook(bundebug.NewQueryHook(
-		bundebug.WithVerbose(true),
-		bundebug.FromEnv("BUNDEBUG"),
-	))
-	return
-}
-
-// endpoint handlers
-func rootRoute(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "hello world", "status": http.StatusOK})
-}
-
-// database models
-type MyBaseModel struct {
-	bun.BaseModel
-	ID        int       `bun:",pk"`
-	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
-	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
-}
-
-type User struct {
-	MyBaseModel `bun:"table:users,alias:u"`
-	Balance     float32
-}
-type Reservation struct {
-	MyBaseModel `bun:"table:reservations,alias:r"`
-	UserID      int
-	User        User `bun:"rel:belongs-to,join:user_id=id"`
-	ProductID   string
-	OrderID     string
-	Price       string
-	Status      string
-}
-
-//database tables
-
-func CreateUserTable(base *bun.DB) {
-	_, err := base.NewCreateTable().
-		Model((*User)(nil)).
-		IfNotExists().
-		Exec(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	return
-}
-
-func CreateReservationsTable(base *bun.DB) {
-	_, err := base.NewCreateTable().
-		Model((*Reservation)(nil)).
-		IfNotExists().
-		Exec(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	return
+	router := webserver.CreateRouterEngine()
+	webserver.InitializeEndpoints(router)
+	db := database.InitializeDBConnection(utils.ParseEnvToDSN())
+	database.CreateTables(db)
+	router.Run()
 }
