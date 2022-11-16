42c54f685154001c981d5e44dcb6c7f7aa652558
ilya <i.kiselevsky@gmail.com>
2022-11-16 21:21:51 +0300

    fix: dsn parser works fine now x2

--- utils/utils.go
+++ utils/utils.go
@@ -7,13 +7,14 @@ import (
 )
 
 
-//DSN = "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOTNAME:5432/db?sslmode=disable"
+//DSN = "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOTNAME:5432/$POSTGRES_DB?sslmode=disable"
 func SerializeDSN() (dsn string) {
 	
-	dsn = fmt.Sprintf("postgres://%s:%s@%s:5432/db&sslmode=disable",
+	dsn = fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=disable",
 					os.Getenv("POSTGRES_USER"),
 	 				os.Getenv("POSTGRES_PASSWORD"),
-					os.Getenv("POSTGRES_HOSTNAME"))
+					os.Getenv("POSTGRES_HOSTNAME"),
+					os.Getenv("POSTGRES_DB"))
 	log.Println(dsn)
 	return
 }
